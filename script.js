3dbe68033e5cb0e8ca05874b3f1bed21aa1aa58a
Christian Henrique <christianhmartins@gmail.com>
2021-07-02 21:49:16 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var nomeCidade = {
+        
+}
+var nomeCidade2 = {
+    Las Vegas
+}
+var roteiroA1
+document.write ('Foram citados ${} locais no Roteiro A de São Paulo') 
+var roteiroA2
+document.write ('Foram citados ${} locais no Roteiro A de Las Vegas')
+var pontoTur1
+
+var pontoTur2
+
+function exiba() {
+}
+
+function pulaLinha() {
+    document.write("<br>");
+}
